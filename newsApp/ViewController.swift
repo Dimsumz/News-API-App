87e6904c39210213a7cb862b367949c7318efc10
Jonathan <jonsouyang@gmail.com>
2023-01-02 12:21:55 -0800

    set up table view with cells error in fetching "author" from API (was not able to find reason) when debugging test every part of struct individually

--- newsApp/ViewController.swift
+++ newsApp/ViewController.swift
@@ -7,15 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let itemArray = ["Item 1", "Item 2", "Item 3"]
+    let cellIdentifier = "cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let url = "https://newsapi.org/v2/everything?q=Apple&from=2023-01-01&sortBy=popularity&apiKey=059a944b5cb9401d852ffb9e841301c4"
+        let url = "https://newsapi.org/v2/everything?q=Apple&from=2023-01-02&sortBy=popularity&apiKey=059a944b5cb9401d852ffb9e841301c4"
         getData(from: url)
         print("Test")
+                
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+    
+    //number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.itemArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: newsViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as! newsViewCell
+        
+        cell.label.text = self.itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("you tapped cell number \(indexPath.row).")
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     //https://newsapi.org/v2/everything?q=Apple&from=2023-01-01&sortBy=popularity&apiKey=059a944b5cb9401d852ffb9e841301c4
 
@@ -47,9 +82,10 @@ class ViewController: UIViewController {
             
             print(json.status)
             print(json.totalResults)
-            print(json.articles[0].author)
+            //print(json.articles)
+            //print(json.title[0].author)
             json.articles.forEach { a in
-                print(a.author)
+                print(a.title)
             }
             
             //print(json.articles.source.id)
@@ -64,14 +100,17 @@ class ViewController: UIViewController {
 struct Response: Codable {
     let status: String
     let totalResults: Int
+    
     let articles: [articleResults]
 }
 
 struct articleResults: Codable {
-    let author: String
+//    let author: String
     let title: String
     let content: String
     let url: String
+    
+
 }
 /*
 struct sourceResults: Codable {
---
+++ newsApp/Views/newsViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  newsViewCell.swift
+//  newsApp
+//
+//  Created by Jonathan Ouyang on 1/1/23.
+//
+
+import Foundation
+import UIKit
+
+class newsViewCell: UITableViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var cellView: UIView!
+    
+    
+}
